0ed73a35110261e53182f42049d90ed04776104c
Todd MacDonald <Toddwmac@gmail.com>
2026-01-31 19:27:51 -0700

    fixing Vercel Build

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbopack: {
-      root: path.join(__dirname),
-    },
-  },
+  /* config options here */
 };
 
 export default nextConfig;
